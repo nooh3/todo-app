f6f32b1a7680fe3019359857a2d146e4eebf8eff
nooh3 <chosh313@naver.com>
2023-08-07 16:22:18 +0900

    기능완성

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
+// import star from './star.png';
+// import './App.css';
+// import Footer from './components/Footer';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={star} className="App-logo" alt="star" />
+//         <p>오늘 뭐 해야하지?</p>
+//       </header>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
 import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlice';
 import './App.css';
+import styled from 'styled-components';
+
+const ChangeValuebutton = styled.button`
+  display: : inline-flex;
+
+  justify-content: center;
+  align-items: center;
+  background-color: #61dafb;
+  font-size: 36px;
+  border: 0;
+  border-radius: 50%;
+  width: 4rem;
+  height: 4rem;
+  margin: 1rem;
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 1rem;
+`;
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <span>{count}</span>
+        <ButtonWrapper>
+          <ChangeValuebutton
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            +
+          </ChangeValuebutton>
+        </ButtonWrapper>
+        <ChangeValuebutton
+          aria-label="decrement value"
+          onClick={() => dispatch(decrement())}
         >
-          Learn React
-        </a>
+          -
+        </ChangeValuebutton>
       </header>
     </div>
   );
